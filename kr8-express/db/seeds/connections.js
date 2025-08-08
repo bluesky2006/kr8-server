41f03fa9882c30eab4eca1c8291b4d4f08295fda
Joel Kram <joelnskram@gmail.com>
2025-08-08 11:06:39 +0100

    fixed connetions

--- kr8-express/db/seeds/connections.js
+++ kr8-express/db/seeds/connections.js
@@ -16,14 +16,6 @@ if (!process.env.PGDATABASE) {
   throw new Error('No PGDATABASE configured');
 }
 
-// Debug: Check what values are being used
-console.log('Database config:', {
-  database: process.env.PGDATABASE,
-  username: process.env.PGUSER,
-  password: process.env.PGPASSWORD ? '[SET]' : '[NOT SET]',
-  host: process.env.PGHOST || 'localhost',
-});
-
 const sequelize = new Sequelize(
   process.env.PGDATABASE, // database name
   null, // username (null = use system default)

03c3a15f2c05d59a48a2862bf2bde514b144c6d1
bluesky2006 <simon.busby@gmail.com>
2025-08-12 14:12:35 +0100

    add CORS

--- kr8-express/app.js
+++ kr8-express/app.js
@@ -3,42 +3,42 @@ const {
   getUserById,
   getPlaylistsByUserId,
   getNestedDataByUserId,
-} = require('./controllers/getUsers.controller');
+} = require("./controllers/getUsers.controller");
 
-const {
-  getAllPlaylists,
-  getPlaylistById,
-} = require('./controllers/getPlaylists.controller');
-const getAllTracks = require('./controllers/getTracks.controller');
+const { getAllPlaylists, getPlaylistById } = require("./controllers/getPlaylists.controller");
+const getAllTracks = require("./controllers/getTracks.controller");
 
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/endpoint.json');
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/endpoint.json");
 }); //test
 
 // -------- USERS -------
 
 //get users
-app.get('/api/users', getAllUsers);
-app.get('/api/users/:id', getUserById);
+app.get("/api/users", getAllUsers);
+app.get("/api/users/:id", getUserById);
 //get playlists by user id
-app.get('/api/users/:id/playlists', getPlaylistsByUserId);
+app.get("/api/users/:id/playlists", getPlaylistsByUserId);
 
 //get full nested data
-app.get('/api/users/:id/playlists/:id/tracks', getNestedDataByUserId);
+app.get("/api/users/:id/playlists/:id/tracks", getNestedDataByUserId);
 //post full nested data
 
 // ----- PLAYLISTS ------
 
 //get playlists
-app.get('/api/playlists', getAllPlaylists);
-app.get('/api/playlists/:id', getPlaylistById);
+app.get("/api/playlists", getAllPlaylists);
+app.get("/api/playlists/:id", getPlaylistById);
 
 // ------ TRACKS ------
 
 //get tracks
-app.get('/api/tracks', getAllTracks);
+app.get("/api/tracks", getAllTracks);
 
 module.exports = app;
